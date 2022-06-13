bc97da0ff7fd9bd48b7fa0667744318ef3aa8dac
diego <dcmikki@gmail.com>
2022-06-13 01:19:42 +0100

    install terraform

--- terraform/terraform_via_bash.sh
+++ terraform/terraform_via_bash.sh
@@ -8,5 +8,8 @@ echo "Add hashicorp offitual repo"
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-echo "Install latest version"
+echo "Install terraform...."
 sudo apt install -y terraform
+
+echo "terraform version"
+terraform version
